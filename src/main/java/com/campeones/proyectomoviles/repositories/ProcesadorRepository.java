bb78ae45c15ff5c1b8fc1059214353c733750eb5
cdominguezh06 <156293143+cdominguezh06@users.noreply.github.com>
2025-03-10 08:43:33 +0100

    Commit final V2

--- src/main/java/com/campeones/proyectomoviles/repositories/ProcesadorRepository.java
+++ src/main/java/com/campeones/proyectomoviles/repositories/ProcesadorRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.campeones.proyectomoviles.model.Entities.Procesador;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProcesadorRepository extends JpaRepository<Procesador, Long>, JpaSpecificationExecutor<Procesador> {
 
 	List<Procesador> findByTipo(String tipo);
--- src/main/java/com/campeones/proyectomoviles/repositories/SolicitudRepository.java
+++ src/main/java/com/campeones/proyectomoviles/repositories/SolicitudRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.campeones.proyectomoviles.model.Entities.Solicitud;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SolicitudRepository extends JpaRepository<Solicitud, Long>, JpaSpecificationExecutor<Solicitud> {
 
 	List<Solicitud> findByRemitenteId(Long id);
